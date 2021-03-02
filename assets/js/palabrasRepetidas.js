16e15e9f906ef29b956bdd500ab513973dc292cc
eva-matus <eva.matus@hotmail.com>
2021-03-01 22:11:22 -0300

    retorno de resultado

--- assets/js/palabrasRepetidas.js
+++ assets/js/palabrasRepetidas.js
@@ -13,21 +13,27 @@ window.oonload = () => {
     }
     const countString = (strings) => {
         let counts = {};
+        
         strings.forEach((string) => {
             counts[string] = (counts[striing] || 0) +1;
         });
         return counts;
+    //Agrega resultado de letras o palabras al DOM
+    const showResults = (word, results, node) => { // function showResults(word, results, node)
+        for (const result in results) { // results.each do |result| 
+            node.insertAdjacentHTML(
+                'beforebegin',
+                `<p>
+                    <strong>${word}: ${result} </strong>
+                    <span>Cantidad: ${results[result]}</span>
+                </p>`
+            )
+        }
     }
-} 
 
-const showResults = (word, results, node) => {
-    for(const result in results) {
-        node.insertaAdjacentHTML(
-            'beforeend',
-            <p>
-                <strong>${word}: ${result} </strong>
-                <span>Cantidad: ${results[result]}</span>
-            </p>
-        )
-    }
+    
+
+    const words = filteredWords(inputText.textContent, /[^A-Za-z]\s/g, ' ');
+    // console.log(filteredWords(inputText.textContent, /[^A-Za-z]\s*/g, ''));
+    showResults('palabra', countString(words), resultText)
 }
